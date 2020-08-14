fb89c8a2965724b6897876c5a69e5374ea4241d5
marcos <marcos.adamczuk@gmail.com>
2020-08-14 18:28:34 -0300

    made models serializable

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,8 @@
+use serde::{Deserialize, Serialize};
+
 pub type Timestamp = u64;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct Message {
     pub sent_ts: Timestamp,
     pub exec_ts: Timestamp,
@@ -11,13 +13,13 @@ pub struct Message {
     pub is_anti: bool,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Checkpoint<State> {
     pub timestamp: Timestamp,
     pub state: State,
 }
 
-#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct ComponentId {
     pub federation_id: u32,
     pub federate_id: u32,
