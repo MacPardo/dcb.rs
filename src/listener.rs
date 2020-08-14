b02e656d1f2898d2d8c694dbd2391a1c290741f1
marcos <marcos.adamczuk@gmail.com>
2020-08-14 19:09:16 -0300

    updated listener

--- src/listener.rs
+++ src/listener.rs
@@ -12,7 +12,7 @@ pub fn run_listener<Addr: ToSocketAddrs>(address: Addr, sender: Sender<Message>)
         stream.read(&mut buffer).unwrap();
         let msg = String::from_utf8(buffer.to_vec()).unwrap();
         let msg = msg.replace('\n', "");
-        let msg = msg.trim().trim_matches(char::from(0));
+        let msg = msg.trim_matches(char::from(0));
         let msg = serde_json::from_str(&msg.to_owned()).unwrap();
         sender.send(msg).unwrap();
     }
