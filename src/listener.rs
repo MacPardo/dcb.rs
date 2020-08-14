7def33e582a70e46c3987eecec04132cc6c31653
marcos <marcos.adamczuk@gmail.com>
2020-08-14 18:17:46 -0300

    udpated listener

--- src/listener.rs
+++ src/listener.rs
@@ -4,11 +4,8 @@ use std::sync::mpsc::Sender;
 
 #[allow(dead_code)]
 pub fn run_listener<Addr: ToSocketAddrs>(address: Addr, sender: Sender<String>) {
-    println!("before listener");
     let listener = TcpListener::bind(address).unwrap();
-    println!("after listener");
     for stream in listener.incoming() {
-        println!("there is incoming");
         let mut stream = stream.unwrap();
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
