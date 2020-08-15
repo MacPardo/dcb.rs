acd4053502150c02944c78794cf732cd0f92552c
marcos <marcos.adamczuk@gmail.com>
2020-08-15 20:51:35 -0300

    updated messenger

--- src/messenger.rs
+++ src/messenger.rs
@@ -2,17 +2,28 @@ use crate::models::{ComponentId, Message};
 use std::collections::HashMap;
 use std::sync::mpsc::{SendError, Sender};
 
-#[allow(dead_code)]
-pub fn get_messenger(
-    local_senders: HashMap<ComponentId, Sender<Message>>,
-    network_sender: Sender<Message>,
-) -> impl FnOnce(Message) -> Result<(), SendError<Message>> {
-    move |msg| {
-        if let Some(sender) = local_senders.get(&msg.to) {
+pub struct Messenger {
+    pub local_senders: HashMap<ComponentId, Sender<Message>>,
+    pub network_sender: Sender<Message>,
+}
+
+impl Messenger {
+    #[allow(dead_code)]
+    pub fn send(&self, msg: Message) -> Result<(), SendError<Message>> {
+        if let Some(sender) = self.local_senders.get(&msg.to) {
             sender.send(msg)?;
         } else {
-            network_sender.send(msg)?;
+            self.network_sender.send(msg)?;
         }
         Ok(())
+    }
+
+    #[allow(dead_code)]
+    pub fn send_local(&self, msg: Message) -> Result<(), SendError<Message>> {
+        if let Some(sender) = self.local_senders.get(&msg.to) {
+            sender.send(msg)?;
+            return Ok(());
+        }
+        Err(SendError(msg))
     }
 }
