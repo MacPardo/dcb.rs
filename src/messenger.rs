cd4c05789a7416d634e2ce999b1a4074deea7b70
marcos <marcos.adamczuk@gmail.com>
2020-08-15 20:06:22 -0300

    updated messenger

--- src/messenger.rs
+++ src/messenger.rs
@@ -1,17 +1,18 @@
 use crate::models::{ComponentId, Message};
 use std::collections::HashMap;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{SendError, Sender};
 
 #[allow(dead_code)]
 pub fn get_messenger(
     local_senders: HashMap<ComponentId, Sender<Message>>,
     network_sender: Sender<Message>,
-) -> impl FnOnce(Message) {
+) -> impl FnOnce(Message) -> Result<(), SendError<Message>> {
     move |msg| {
         if let Some(sender) = local_senders.get(&msg.to) {
-            sender.send(msg).unwrap();
+            sender.send(msg)?;
         } else {
-            network_sender.send(msg).unwrap();
+            network_sender.send(msg)?;
         }
+        Ok(())
     }
 }
