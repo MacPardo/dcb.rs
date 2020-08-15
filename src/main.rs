d221c24d8298e69b42a235ac974e1bfcd658056e
marcos <marcos.adamczuk@gmail.com>
2020-08-15 01:28:14 -0300

    cleaned main

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,4 @@ mod models;
 mod network;
 mod rollback_manager;
 
-use std::collections::BinaryHeap;
-
-fn main() {
-    let mut h = BinaryHeap::new();
-    h.push(2);
-    h.push(2);
-
-    println!("{:?}", h.pop());
-    println!("{:?}", h.pop());
-}
+fn main() {}
